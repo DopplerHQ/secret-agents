816f58641cb2c75f4801feb315f89039ffdeac05
Nic Manoogian <nic.manoogian@doppler.com>
2022-10-20 18:18:08 -0400

    Install main dependencies during deployment

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -3,4 +3,4 @@ set -eo pipefail
 
 # Because the script will be run without a `package.json`, it cannot specifify `"type": "module"`.
 # The `{"module": "CommonJS"}` option modifier tell ts-node to transpile our ESM imports to CJS before execution.
-ts-node -O '{"module": "CommonJS"}' ./deployment/deploy.ts
+./node_modules/.bin/ts-node -O '{"module": "CommonJS"}' ./deployment/deploy.ts
